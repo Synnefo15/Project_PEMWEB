fcb47b27823f921594bcd7fca9e2ca1ec445e612
muhammad rafi cahyaputra <71932553+Synnefo15@users.noreply.github.com>
2021-06-11 17:11:39 +0700

    Create config.php

---
+++ app/config/config.php
@@ -0,0 +1,8 @@
+<?php
+define('BASEURL', 'http://localhost/Project_PEMWEB/public');
+
+// * DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'pemwebprojek');
