d0b94d5ff422e3f9997d7d81fbfb743f2f292cfb
muhammad rafi cahyaputra <71932553+Synnefo15@users.noreply.github.com>
2021-06-14 18:04:02 +0700

    Update detail.php

--- app/views/barang/detail.php
+++ app/views/barang/detail.php
@@ -1,8 +1,8 @@
-<div class="container mt-5">
+<div class="container mt-5 p-5">
 	<div class="card" style="width: 18rem;">
 		<div class="card-body">
-			<h5 class="card-title"><?= $data['mhs']['nama_kota']; ?></h5>
-			<h6 class="card-subtitle mb-2 text-muted"><?= $data['mhs']['provinsi']; ?></h6>
+			<h5 class="card-title"><?= $data['kota']['nama_kota']; ?></h5>
+			<h6 class="card-subtitle mb-2 text-muted"><?= $data['kota']['provinsi']; ?></h6>
 			<a href="<?= BASEURL; ?>/barang" class="card-link">Kembali</a>
 		</div>
 	</div>
